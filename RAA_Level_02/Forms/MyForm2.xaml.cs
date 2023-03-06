667909b63efa5b04fa846c3682d5e87148a677f9
Tom E Flaherty <teflaherty@utexas.edu>
2023-03-05 20:55:17 -0600

    in progress

--- RAA_Level_02/Forms/MyForm2.xaml.cs
+++ RAA_Level_02/Forms/MyForm2.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -23,12 +24,27 @@ namespace RAA_Level_02_Skills
     /// </summary>
     public partial class MyForm2 : Window
     {
+        public Document myDoc;
         public MyForm2(string testText, Document doc)
         {
-            InitializeComponent();
+            InitializeComponent()
+
+            myDoc = doc;
 
             lblLabel.Content = testText + doc.PathName;
         }
 
+        public void DocumentTest()
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(myDoc);
+            collector.OfCategory(BuiltInCategory.OST_Views);
+
+            TaskDialog.Show("Test", "There are " + collector.Count.ToString() + " views in the model.");
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            DocumentTest();
+        }
     }
 }
