907a54d629b05ae91cfc280035824e99ea9620cb
Tom E Flaherty <teflaherty@utexas.edu>
2023-03-29 19:01:12 -0500

    complete

--- RAA_Level_02/Command2.cs
+++ RAA_Level_02/Command2.cs
@@ -4,11 +4,13 @@ using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
+using RAA_Level_02_Skills.Forms;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Windows.Controls;
 
 #endregion
 
@@ -27,47 +29,67 @@ namespace RAA_Level_02_Skills
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
+            Units curUnits = doc.GetUnits();
+            FormatOptions curFormatOptions = curUnits.GetFormatOptions(SpecTypeId.Length);
+            ForgeTypeId typeId = curFormatOptions.GetUnitTypeId();
+
+            if (typeId == UnitTypeId.Meters)
+                TaskDialog.Show("Units", "The current model is metric.");
+            else if (typeId == UnitTypeId.Millimeters)
+                TaskDialog.Show("Units", "The current model is metric.");
+            else if (typeId == UnitTypeId.Feet)
+                TaskDialog.Show("Units", "The current model is imperial.");
+            else if (typeId == UnitTypeId.Inches)
+                TaskDialog.Show("Units", "The current model is imperial.");
+            else if (typeId == UnitTypeId.FeetFractionalInches)
+                TaskDialog.Show("Units", "The current model is imperial.");
+            else
+            {
+                TaskDialog.Show("Units", "The current model is something else.");
+            }
+
+
             // step 1: put any code needed for the form here
 
             // step 2: open form
 
-            List<string> args = new List<string> { "abc", "bcd", "cde", "def" };
+            //List<string> args = new List<string> { "abc", "bcd", "cde", "def" };
 
-            MyForm2 curForm = new MyForm2("", doc, null)
-            {
-                Width = 500,
-                Height = 450,
-                WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen,
-                Topmost = true,
-            };
+            //MyForm2 curForm = new MyForm2("", doc, null)
+            //{
+            //    Width = 500,
+            //    Height = 450,
+            //    WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen,
+            //    Topmost = true,
+            //};
 
-            curForm.ShowDialog();
+            //curForm.ShowDialog();
 
-            List<Reference> refList = new List<Reference>();
-            bool flag = true;
+            //List<Reference> refList = new List<Reference>();
+            ////bool flag = true;
 
-            try
-            {
-                refList = uidoc.Selection.PickObjects(ObjectType.Element, "Pick some items.").ToList<Reference>();
-            }
-            catch (Exception)
-            {
+            //try
+            //{
+            //    refList = uidoc.Selection.PickObjects(ObjectType.Element, "Pick some items.").ToList<Reference>();
+            //}
+            //catch (Exception)
+            //{
 
-            }
+            //}
 
-            List<Viewport> viewportList = new List<Viewport>();
-            
-            foreach(Reference curRef in refList)
-            {
-                Element curElem = doc.GetElement(curRef);
+            //List<Viewport> viewportList = new List<Viewport>();
 
-                if(curElem is Viewport)
-                {
-                    Viewport curVP = curElem as Viewport;
-                    viewportList.Add(curVP);
-                }                
-            }
-                  
+            //foreach(Reference curRef in refList)
+            //{
+            //    Element curElem = doc.GetElement(curRef);
+
+            //    if(curElem is Viewport)
+            //    {
+            //        Viewport curVP = curElem as Viewport;
+            //        viewportList.Add(curVP);
+            //    }                
+            //}
+
             //while (flag == true)
             //{
             //    try
@@ -82,10 +104,10 @@ namespace RAA_Level_02_Skills
             //    }
             //}
 
-            string cmbString = "There are " + refList.Count.ToString() + " selected elements";
-            List<string> lbxString = curForm.GetSelectedListBoxItems();
+            //string cmbString = "There are " + refList.Count.ToString() + " selected elements";
+            //List<string> lbxString = curForm.GetSelectedListBoxItems();
 
-            MyForm2 curForm2 = new MyForm2(cmbString, doc, lbxString)
+            Forms.MyForm3 curForm = new Forms.MyForm3()
             {
                 Width = 500,
                 Height = 450,
@@ -93,13 +115,15 @@ namespace RAA_Level_02_Skills
                 Topmost = true,
             };
 
-            curForm2.ShowDialog();
+            curForm.ShowDialog();
 
-            string cmbString2 = curForm2.GetSelectedComboBoxItem();
-            List<string> lbxString2 = curForm2.GetSelectedListBoxItems();
+            TaskDialog.Show("Test", "There were " + curForm.GetSelectedItems().Count.ToString() + " items selected");
 
+            //string cmbString2 = curForm2.GetSelectedComboBoxItem();
+            //List<string> lbxString2 = curForm2.GetSelectedListBoxItems();
+                  
             return Result.Succeeded;
-        }
+        }       
 
         public static String GetMethod()
         {
---
+++ RAA_Level_02/Forms/MyForm3.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RAA_Level_02_Skills.Forms
+{
+    /// <summary>
+    /// Interaction logic for MyForm3.xaml
+    /// </summary>
+    public partial class MyForm3 : Window
+    {
+        public MyForm3()
+        {
+            InitializeComponent();
+
+            lbxTest.Items.Add("abc");
+
+            lbxTest.Items.Add("bcd");
+
+            lbxTest.Items.Add("cde");
+
+            lbxTest.Items.Add("def");
+
+            lbxTest.Items.Add("efg");
+        }
+
+        public List<string> GetSelectedItems()
+        {
+            List<string> returnList = new List<string>();
+
+            foreach (string item in lbxTest.SelectedItems) 
+            {
+                returnList.Add(item);
+            }
+
+            return returnList;
+        }
+    }
+}
